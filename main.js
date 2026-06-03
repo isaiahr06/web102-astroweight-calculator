4e9aa44d414868a1eedc446e3a773c99a457b3f1
Isaiah Rodriguez <irod196@wgu.edu>
2026-06-02 19:14:19 -0500

    Created a Loop to append each planet as an option, Caluclate weight on selected planet, created function that reacts on button click.

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 // Write your JavaScript code here!
+var planets = [ 
+        ['Pluto', 0.06], 
+        ['Neptune', 1.148], 
+        ['Uranus', 0.917], 
+        ['Saturn', 1.139], 
+        ['Jupiter', 2.640], 
+        ['Mars', 0.3895], 
+        ['Moon', 0.1655], 
+        ['Earth', 1], 
+        ['Venus', 0.9032], 
+        ['Mercury', 0.377], 
+        ['Sun', 27.9] 
+    ];
+
+let dropdown = document.getElementById('planet');
+
+planets.forEach(function(planet){
+    let option = document.createElement('option');
+    option.value = planet[0];
+    option.textContent = planet[0];
+    dropdown.appendChild(option);
+})
+
+function calculateWeight(weight, planetName){
+    for(let planet of planets){
+        if(planet[0] == planetName){
+            return weight * planet[1];
+        }
+    }
+}
+function handleClickEvent(){
+    let userWeight = document.getElementById('user-weight').value;
+    let planetName = document.getElementById('planet').value;
+    let result = calculateWeight(userWeight, planetName);
+    let output = document.getElementById('output');
+    output.textContent = "If you were on " + planetName + " you would weigh " + result + "lbs";
+}
